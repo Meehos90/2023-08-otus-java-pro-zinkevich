6bc28cb5e4060139e023c22df69cab0aa34848f7
Зинкевич Михаил Николаевич <mnzinkev@mts.ru>
2023-09-04 12:11:29 +0300

    add: hw01-gradle

---
+++ hw01-gradle/src/test/java/ru/otus/pro/hw01gradle/HelloOtusTests.java
@@ -0,0 +1,13 @@
+package ru.otus.pro.hw01gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloOtusTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
